47f93723d3bade172e3dab5c59902c95cd17110e
Meija Lohiniva <meija.lohiniva@seamk.fi>
2020-10-05 10:57:34 +0300

    Added sensurointi-task

---
+++ vko2/sensurointi.js
@@ -0,0 +1,4 @@
+let paragraphs = document.getElementsByTagName('p');
+for ( let i = 0; i < paragraphs.length; i++ ) {
+    paragraphs[i].innerHTML = paragraphs[i].innerHTML.replaceAll("ac", "##");
+}
